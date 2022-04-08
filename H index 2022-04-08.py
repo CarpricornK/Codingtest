a9dbf2c9a7071d0b17b6a79125a1e8f9a710063f
CarpricornK <93844704+CarpricornK@users.noreply.github.com>
2022-04-08 10:49:32 +0900

    Add files via upload

---
+++ H index 2022-04-08.py
@@ -0,0 +1,15 @@
+def solution(citations):
+    print(citations)
+    print(type(citations))
+    citations.sort(reverse=True)
+
+    print(citations)
+
+    for idx, citation in enumerate(citations):
+
+        print("idx : ", idx)
+        print("citation : ", citation)
+
+        if idx >= citation:
+            return idx
+    return answer
